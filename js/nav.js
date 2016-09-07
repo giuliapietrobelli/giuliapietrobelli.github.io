d2900767f8c42fe235d56a5a3e4e315b1919e2bc
Alessandro Rosa <xelasor@hotmail.it>
2016-09-07 16:45:07 +0200

    page title now changes accordingly to navigation

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 (function($, window, undefined) {
   'use strict';
   var $pages = [];
+  var $title;
   var history = window.history;
   var location = window.location;
   var addEventListener = window.addEventListener;
@@ -21,29 +22,49 @@
     // The page requested exist
     if($visiblePage.length === 1) {
       $visiblePage.show();
-      // history.pushState({path: path}, title, path);
+      changeTitle(path);
       return;
     }
 
     // The page requested doesn't exist but the default homepage is set
     if($homePage.length === 1) {
-      history.replaceState({path: '/'}, 'Homepage', '/');
+      history.replaceState({path: '/'}, null, '/');
       $homePage.show();
+      changeTitle();
       return;
     }
   };
 
+  var findTitleFromPath = function(path) {
+    if(path !== '/') {
+      var title = path.trim().slice(1, path.length);
+      title = title.charAt(0).toUpperCase() + title.slice(1);
+      return title;
+    } else {
+      return '';
+    }
+  };
+
+  var changeTitle = function(path) {
+    var section = findTitleFromPath(path);
+    var base = $title.text().split('-')[0].trim();
+
+    if(section.length > 0) {
+      $title.text(base + ' - ' + section);
+    } else {
+      $title.text(base)
+    }
+  };
 
   $('document').ready(function(){
     $pages = $('body > section');
+    $title = $('head title');
 
     addEventListener('popstate', function(event) {
-      console.log(event.state);
       if(event.state) {
         changePageTo(event.state.path);
       }
     });
-
 
     var currentPath = location.pathname ? location.pathname : '/';
     history.replaceState({path: currentPath}, null, currentPath);
@@ -53,16 +74,10 @@
   $('.nav').on('click', function(e) {
     e.preventDefault();
     var pagePath = $(e.target).attr('href');
-    var title = 'Homepage';
 
     if(!pagePath) return;
 
-    if(pagePath !== '/') {
-      title = pagePath.trim().slice(1, pagePath.length);
-      title = title.charAt(0).toUpperCase() + title.slice(1);
-    };
-
+    history.pushState({path: pagePath}, null, pagePath);
     changePageTo(pagePath);
-    history.pushState({path: pagePath}, title, pagePath);
   });
 }(jQuery, window, undefined));
