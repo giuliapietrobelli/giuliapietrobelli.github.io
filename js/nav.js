24b768e8844823ed472bbce15f2b6d5bd179c1e8
giuliapietrobelli <giuliapietrobelli@gmail.com>
2016-12-04 23:57:30 +0000

    mail activated

--- js/nav.js
+++ js/nav.js
@@ -320,7 +320,7 @@
       }
     });
 
-    $('.nav').on('click', function(e) {
+    $('.nav a:not(.email)').on('click', function(e) {
       e.preventDefault();
       var $target = $(e.target);
       var reqPath = $target.attr('href');
